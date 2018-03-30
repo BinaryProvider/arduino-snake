e64734b229c71fdb4c630250db9d879916ee802a
John Hellbom <john@johnhellbom.com>
2018-03-30 21:31:30 +0200

    Update git ignore

--- Debug/core/abi.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\John\AppData\Local\Temp\VMBuilds\arduino_snake\uno\Debug\core\abi.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\abi.cpp
--- Debug/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\John\AppData\Local\Temp\VMBuilds\arduino_snake\uno\Debug\core\hooks.c.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\hooks.c
